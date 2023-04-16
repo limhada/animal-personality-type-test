0fdbd5ee87123dbc8cd5c3a9ee66f3db8137f053
limhada <leem1315@gmail.com>
2023-04-16 19:30:36 +0900

    ✨  분석기능 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./routes/Routes";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/Routes';
+import RouteChangeTracker from './routes/RouteChangeTracker';
 /*
 ./는 현재 디렉토리를 나타내고, ../는 상위 디렉토리를 나타냅니다. 따라서 import Router from "./routes/Routes";는 현재 디렉토리의 routes/Routes 모듈을 import하고, import Router from "../routes/Routes";는 상위 디렉토리의 routes/Routes 모듈을 import합니다.
 */
 function App() {
+  RouteChangeTracker();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Router/>
-      </BrowserRouter>
-    </div >
+      
+        <Router />
+      {/* </BrowserRouter> */}
+    </div>
   );
 }
 
