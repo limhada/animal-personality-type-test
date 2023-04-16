2de5bd3de331d499c2bf342d57323fefc0150112
limhada <leem1315@gmail.com>
2023-04-16 22:30:00 +0900

    🔥 라우터 수정

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { useRoutes } from "react-router-dom"; 
-/* Navigate*/
-import Main from "../pages/Main";
-import One from "../pages/One";
+import { useRoutes, Navigate } from 'react-router-dom';
+import Main from '../pages/Main';
+import One from '../pages/One';
 
-export default function Router() {
-  const routes = useRoutes([
-    {
-      path: "/",
-      element: <Main />,
-    },
-    {
-      path: "/2",
-      element: <One />,
-    },
-    
+const AppRoutes = () => {
+  const routing = useRoutes([
+    { path: '/', element: <Main /> },
+    { path: '/1', element: <One /> },
+    { path: '*', element: <Navigate to="/" /> },
   ]);
-  return routes;
-}
+  return routing;
+};
+
+export default AppRoutes;
