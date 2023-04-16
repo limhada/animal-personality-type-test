ebe716467d5249ff4273e1873ab61d55262536f7
limhada <leem1315@gmail.com>
2023-04-16 23:42:54 +0900

    🐛 구글애널리틱스-수정2

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga'
 import { BrowserRouter } from 'react-router-dom';
 
-const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
