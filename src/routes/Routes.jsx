6a1df58be71b64350a59e0d4327dcce2e06765d2
limhada <leem1315@gmail.com>
2023-04-21 17:42:49 +0900

    수글 애널리틱스 수정

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,7 +6,7 @@ import One from '../pages/One';
 const AppRoutes = () => {
   const routing = useRoutes([
     { path: '/', element: <Main /> },
-    { path: '/2', element: <One /> },
+    // { path: '/2', element: <One /> },
     { path: '*', element: <Navigate to="/" /> },
   ]);
   return routing;
